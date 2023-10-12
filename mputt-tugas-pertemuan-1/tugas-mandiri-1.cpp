568d58e80be449023c86e3735150eb468e8c989a
mputttt <147709696+mputttt@users.noreply.github.com>
2023-10-12 02:56:11 -0700

    Add files via upload

---
+++ mputt-tugas-pertemuan-1/tugas-mandiri-1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int nim = 23341;
+	char* nama = "Masalalu Dia";
+	char* jurusan = "Informatika";
+	
+	printf("nim : %i \n", nim);
+	printf("nama : %s \n", nama);
+	printf("jurusan : %s", jurusan);
+	return 0;
+}
